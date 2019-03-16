357f71af3f27f200beed08f43ceee908d978de5a
ckg <chagmgang@gmail.com>
2019-03-16 18:42:26 +0900

    add ppo bipedal

---
+++ bipedalwalker_ppo.py
@@ -0,0 +1,79 @@
+
+import gym
+import tensorflow as tf
+from tensorboardX import SummaryWriter
+from agent.continuous.seperate.ppo import PPO
+from agent.utils import get_gaes
+from example_model.policy.mlp.continuous import MLPContinuousActor
+from example_model.policy.mlp.continuous import MLPContinuousCritic
+
+env = gym.make('BipedalWalker-v2')
+sess = tf.Session()
+state_size = 24
+output_size = 4
+actor = MLPContinuousActor('actor', state_size, output_size)
+critic = MLPContinuousCritic('critic', state_size)
+agent = PPO(sess, state_size, output_size, actor, critic)
+sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+#saver.restore(sess, 'model/model')
+
+writer = SummaryWriter()
+
+values, states, actions, dones, logp_ts, rewards = [], [], [], [], [], []
+o, r, d, ep_ret, ep_len = env.reset(), 0, False, 0, 0
+
+rollout = 0
+score = 0
+ep = 0
+n_step = 512
+record_score_size = 10
+record_score = 0
+
+while True:
+    score_rollout = 0
+    rollout += 1
+    for t in range(n_step):
+        #if ep % 10 == 0:
+        #    env.render()
+        a, v_t, logp_t = agent.get_action([o])
+        a, v_t, logp_t = a[0], v_t[0], logp_t[0]
+        n_o, r, d, _ = env.step(a)
+
+        score += r
+        score_rollout += r
+        record_score += r
+
+        values.append(v_t)
+        states.append(o)
+        actions.append(a)
+        dones.append(d)
+        rewards.append(r)
+        logp_ts.append(logp_t)
+
+        o = n_o
+
+        if d:
+            ep += 1
+            if ep % record_score_size == 0:
+                if int(ep / record_score_size) < 600:
+                    writer.add_scalar('data/reward', record_score / record_score_size, int(ep / record_score_size))
+                    record_score = 0
+            writer.add_scalar('data/reward_per_episode', score, ep)
+            print(score, ep)
+            score = 0
+            o, r, d, ep_ret, ep_len = env.reset(), 0, False, 0, 0
+
+    a, v_t, logp_t = agent.get_action([o])
+    values.append(v_t[0])
+    next_value = values[1:]
+    value = values[:-1]
+    adv, target = get_gaes(rewards, dones, value, next_value, agent.gamma, agent.lamda, False)
+    value_loss, kl, ent = agent.update(states, actions, target, adv, logp_ts)
+
+    writer.add_scalar('data/value_loss_per_rollout', value_loss, rollout)
+    writer.add_scalar('data/kl_per_rollout', kl, rollout)
+    writer.add_scalar('data/ent_per_rollout', ent, rollout)
+    writer.add_scalar('data/reward_per_rollout', score_rollout, rollout)
+
+    values, states, actions, dones, logp_ts, rewards = [], [], [], [], [], []
--- example_model/policy/mlp/continuous.py
+++ example_model/policy/mlp/continuous.py
@@ -1,5 +1,62 @@
 import tensorflow as tf
 import numpy as np
+from agent.utils import gaussian_likelihood
+
+class MLPContinuousActor:
+    def __init__(self, name, state_size, output_size):
+        self.state_size = state_size
+        self.output_size = output_size
+
+        with tf.variable_scope(name):
+            self.input = tf.placeholder(tf.float32, shape=[None, self.state_size])
+            self.action = tf.placeholder(tf.float32, shape=[None, self.output_size])
+
+            self.l1 = tf.layers.dense(inputs=self.input, units=128, activation=tf.nn.relu)
+            self.l2 = tf.layers.dense(inputs=self.l1,    units=128, activation=tf.nn.relu)
+            self.l3 = tf.layers.dense(inputs=self.l2,    units=128, activation=tf.nn.relu)
+
+            self.mu = tf.layers.dense(inputs=self.l3,    units=self.output_size, activation=None)
+            self.log_std = tf.get_variable(name='log_std', initializer= -0.5 * np.ones(self.output_size, dtype=np.float32))
+            self.std = tf.exp(self.log_std)
+            self.pi = self.mu + tf.random_normal(tf.shape(self.mu)) * self.std
+            self.logp = gaussian_likelihood(self.action, self.mu, self.log_std)
+            self.logp_pi = gaussian_likelihood(self.pi, self.mu, self.log_std)
+    
+            self.scope = tf.get_variable_scope().name
+
+    def get_action_prob(self, obs):
+        return self.sess.run(self.act_probs, feed_dict={self.obs: obs})
+
+    def get_variables(self):
+        return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, self.scope)
+
+    def get_trainable_variables(self):
+        return tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope)
+
+class MLPContinuousCritic:
+    def __init__(self, name, state_size):
+        self.state_size = state_size
+        
+        with tf.variable_scope(name):
+            self.input = tf.placeholder(tf.float32, shape=[None, self.state_size])
+
+            self.l1 = tf.layers.dense(inputs=self.input, units=128, activation=tf.nn.relu)
+            self.l2 = tf.layers.dense(inputs=self.l1,    units=128, activation=tf.nn.relu)
+            self.l3 = tf.layers.dense(inputs=self.l2,    units=128, activation=tf.nn.relu)
+            
+            self.value = tf.layers.dense(inputs=self.l3, units=1,   activation=None)
+            self.v = tf.squeeze(self.value, axis=1)
+
+            self.scope = tf.get_variable_scope().name
+
+    def get_action_prob(self, obs):
+        return self.sess.run(self.act_probs, feed_dict={self.obs: obs})
+
+    def get_variables(self):
+        return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, self.scope)
+
+    def get_trainable_variables(self):
+        return tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope)
 
 class MLPDDPGContinuousActor:
     def __init__(self,name,state_size,action_size):
